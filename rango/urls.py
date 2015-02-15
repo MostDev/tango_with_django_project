63d6c71f3eb18ea88930afd3389965b79a88e0da
mostdev <wim@mostdev.com>
2015-02-16 00:01:53 +0100

    setup rango app in project

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,7 @@
-__author__ = 'wim'
-
 from django.conf.urls import patterns, url
 
 from rango import views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'))
+                       url(r'^$', views.index, name='index')),
